656d510ba71a2e8210c427148f1a10c1ac605393
karthikrk705 <karthikrk705@gmail.com>
2023-09-11 00:32:56 +0530

    002

---
+++ src/components/firstpage.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Frontpage()
+{
+    return(
+        <>
+        <h1>first page</h1>
+        <Link to='/'>Loginpage</Link>
+        </>
+    )
+}
+
+export default Frontpage;
---
+++ src/components/login.js
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import loginsty from '../styles/loginpage.module.css';
+
+function Loginpage()
+{
+    return(
+        <div className={loginsty.background}>
+            <div className="">
+            </div>  
+        </div> 
+    )
+}
+
+export default Loginpage;
+
+{/* <Link to='/frontpage'>mainpage</Link> */}

2cf323d9d30e2d7e4390b74584b295993ba2a899
karthikrk705 <karthikrk705@gmail.com>
2023-09-11 18:57:40 +0530

    004

--- src/components/login.js
+++ src/components/login.js
@@ -1,27 +1,47 @@
 import { Link } from "react-router-dom";
 import loginsty from '../styles/loginpage.module.css';
-
-const password = 12345678
-const username = "king"
-
-function validate(event)
-{
-    event.preventDefault();
-}
+import { useState } from "react";
 
 function Loginpage()
 {
+
+
+
+    const [username, setusername] = useState("")
+    const [password, setpassword] = useState("")
+
+    const [login, setlogin] = useState(false)
+
+    function validate(event)
+    {
+    event.preventDefault();
+    if(username === "testuser" && password === "12345678") 
+    {
+        setlogin(true)
+    }
+    else
+    {
+        setlogin(false)
+    }
+    }
+
+
     return(
         <div className={loginsty.background}>
             <h1 className={loginsty.logo} >TOOL BOX</h1>
             {/* <Link to='/frontpage'>mainpage</Link>  */}
             <div className={loginsty.loginbox}>
                 <form onSubmit={validate}>
-                    <label>username:</label><br/>
-                    <input type="text" className={loginsty.inputboxs}/><br/>
-                    <label>password:</label><br/>
-                    <input type="password" className={loginsty.inputboxs}/><br/>
-                    <input type="submit" value={"Login"} className={loginsty.loginbtn}/><br/>
+                    <div className={loginsty.username}>
+                    <label className={loginsty.inputnames}>username:</label><br/>
+                    <input type="text" className={loginsty.inputboxs} name="un" onChange={(e) => setusername(e.target.value)}/><br/>
+                    </div>
+                    <div className={loginsty.password}>
+                    <label className={loginsty.inputnames}>password:</label><br/>
+                    <input type="password" className={loginsty.inputboxs} name="p" onChange={(e) => setpassword(e.target.value)}/><br/>
+                    </div>
+
+                    {login? (<Link to='/frontpage'><input type="submit" value={"Login"} className={loginsty.loginbtn}/></Link>): (<input type="submit" value={"Login"} className={loginsty.loginbtn}/>)}
                 </form>
             </div>  
         </div> 

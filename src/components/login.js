2aec0e58ab8a70bc148e094f69385b63ff3892dd
karthikrk705 <karthikrk705@gmail.com>
2023-09-11 14:23:56 +0530

    003

--- src/components/login.js
+++ src/components/login.js
@@ -1,11 +1,28 @@
 import { Link } from "react-router-dom";
 import loginsty from '../styles/loginpage.module.css';
 
+const password = 12345678
+const username = "king"
+
+function validate(event)
+{
+    event.preventDefault();
+}
+
 function Loginpage()
 {
     return(
         <div className={loginsty.background}>
-            <div className="">
+            <h1 className={loginsty.logo} >TOOL BOX</h1>
+            {/* <Link to='/frontpage'>mainpage</Link>  */}
+            <div className={loginsty.loginbox}>
+                <form onSubmit={validate}>
+                    <label>username:</label><br/>
+                    <input type="text" className={loginsty.inputboxs}/><br/>
+                    <label>password:</label><br/>
+                    <input type="password" className={loginsty.inputboxs}/><br/>
+                    <input type="submit" value={"Login"} className={loginsty.loginbtn}/><br/>
+                </form>
             </div>  
         </div> 
     )
@@ -13,4 +30,3 @@ function Loginpage()
 
 export default Loginpage;
 
-{/* <Link to='/frontpage'>mainpage</Link> */}
